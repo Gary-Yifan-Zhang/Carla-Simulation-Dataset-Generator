91dfb5aa1984c9f2a629dfb996d0b1f26186372b
Yifan Zhang <3037900395@qq.com>
2025-03-16 14:55:39 +0100

    refactor: update calibration matrix handling to support multiple sensors

--- DatasetSave.py
+++ DatasetSave.py
@@ -190,7 +190,8 @@ class DatasetSave:
 
             save_ref_files(self.OUTPUT_FOLDER, self.captured_frame_no)
 
-            save_calibration_matrices([camera_transform, lidar_transform], calib_filename, dt["intrinsic"])
+            save_calibration_matrices(extrinsic, calib_filename, dt["intrinsic"])
+            # save_calibration_matrices([camera_transform, lidar_transform], calib_filename, dt["intrinsic"])
             save_image_data(img_filename_0, dt["sensor_data"][0])
             save_image_data(img_filename_1, dt["sensor_data"][4])
             save_image_data(img_filename_2, dt["sensor_data"][5])
